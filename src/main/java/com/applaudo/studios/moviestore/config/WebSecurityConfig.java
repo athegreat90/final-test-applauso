7d585b234ead67b59a97ab5c6707a09b7dc114ff
Alvaro Agudelo <alvaro.agudelo@fonyou.com>
2020-11-29 22:30:52 -0500

    Fix WebAuthConfig

--- src/main/java/com/applaudo/studios/moviestore/config/WebSecurityConfig.java
+++ src/main/java/com/applaudo/studios/moviestore/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -56,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/api/v1/auth/signup", "/api/v1/auth/login").permitAll().antMatchers(, "/actuator", "/actuator/*").hasRole("ADMIN")
+                .antMatchers("/api/v1/auth/signup", "/api/v1/auth/login").permitAll().antMatchers( "/actuator", "/actuator/*").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
